8c5d67989b18a3b3f6123b892f4f66e661e014d8
byMaxKucher <63538024+MaxDevIos@users.noreply.github.com>
2021-08-30 19:43:23 +0300

    Arrangement of interface elements

---
+++ PersonalQuizProject/Models/MathsPracticeModel.swift
@@ -0,0 +1,8 @@
+//
+//  MathsPracticeModel.swift
+//  PersonalQuizProject
+//
+//  Created by Maxim Kucherov on 30.08.2021.
+//
+
+
---
+++ PersonalQuizProject/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IntroductionViewController.swift
+//  PersonalQuizProject
+//
+//  Created by Maxim Kucherov on 30.08.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+    
+}
+
