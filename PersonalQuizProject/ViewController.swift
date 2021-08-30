4fbf8f5886e2e1427261a04aa2b86b2d7891c338
byMaxKucher <63538024+MaxDevIos@users.noreply.github.com>
2021-08-30 15:25:08 +0300

    Initial Commit

---
+++ PersonalQuizProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuizProject
+//
+//  Created by Maxim Kucherov on 30.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
